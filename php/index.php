f35b422c65a305784e5cef00749b56d755fdad3c
timothee-l <tim.levilly@gmail.com>
2022-02-28 11:02:36 +0100

    Ajout readme

--- php/index.php
+++ php/index.php
@@ -5,9 +5,4 @@ $smarty = init();
 $smarty->assign('var1','test_var');
 $smarty->debugging = true;
 
-$smarty->setTemplateDir('../smarty/templates/');
-$smarty->setCompileDir('../smarty/templates_c/');
-$smarty->setConfigDir('../smarty/configs/');
-$smarty->setCacheDir('../smarty/cache/');
-
 $smarty->display('../html/index.tpl');
